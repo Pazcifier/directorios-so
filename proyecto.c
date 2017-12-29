6553bac2ba217c0d5cab3a8fb598f4ed6b5a37f0
Angel Rivero <angelantonio3000@gmail.com>
2017-12-28 21:34:12 -0400

    archivo c con el listado del arbol de directorios

---
+++ proyecto.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+void listar(const char *name, int indent)
+{
+    DIR *dir;
+    struct dirent *entry; //Entrada del directorio a iterar
+
+    if (!(dir = opendir(name))) // Si no existe el directorio, se sale
+        return;
+
+    while ((entry = readdir(dir)) != NULL) { // ciclo para leer el directorio hasta que no haya osubdirectorios o archivos
+        if (entry->d_type == DT_DIR) { // Si el tipo del archivo es un directorio entonces entra
+            char path[1024]; // especificamos un array de char con el nombre del directorio
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                continue; // comparamos que el nombre sea igual
+            snprintf(path, sizeof(path), "%s/%s", name, entry->d_name); 
+            printf("%*s[%s]\n", indent, "", entry->d_name);
+            listar(path, indent + 2);
+        }
+    }
+    closedir(dir);
+}
+
+int main(void) {
+    listar(".", 0);
+    return 0;
+}
