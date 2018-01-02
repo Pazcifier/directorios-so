a8381b4347fdc891d293ec2aef389cc613deb76e
José <jpaz17@hotmail.com>
2018-01-02 19:47:29 -0400

    Ayuda

--- pal.c
+++ pal.c
@@ -7,18 +7,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-struct letra {
-  int nroDirectorio;
-  int finDirectorio;
-  char letraDirectorio[255];
-};
+
+char palabra[256][256]; //Variable para guardar las letras
+int contador = 0;     //Variable para guardar la posicion de las letras
 
 void listar(const char *name, int indent)
 {
     DIR *dir;
     struct dirent *entry; //Entrada del directorio a iterar
-    struct letra letras[20];
-    int contDirectorio = 0;
 
     if (!(dir = opendir(name))) // Si no existe el directorio, se sale
         return;
@@ -29,24 +25,73 @@ void listar(const char *name, int indent)
             if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
                 continue; // comparamos que el nombre sea igual
             snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
-            printf("%*s[%s]\n", indent, "", entry->d_name);
-            strncpy(letras[contDirectorio].letraDirectorio, entry->d_name, 254);
-            letras[contDirectorio].nroDirectorio = contDirectorio++;
+            //printf("%*s[%s]\n", indent, "", entry->d_name);
+            strcpy(palabra[contador], entry->d_name); //Agrega la letra en el array
+            contador++;
+            //printf("Path: %s\n", path); Path para revisar como recorre los directorios
             listar(path, indent + 2);
         }
     }
     closedir(dir);
+    strcpy(palabra[contador], "!"); //Agrega un "!" para indicar fin de rama
+    contador++;
+}
 
-    letras[contDirectorio].finDirectorio = 1;
 
-    for (int i = 0; i < contDirectorio; i++) {
-      printf("%s\n", letras[i].letraDirectorio);
+//IGNORAR
+void imprimir(char unaPalabra[][256]) {
+
+  int len = sizeof(unaPalabra)/sizeof(char);
+
+  for (int i = 0; i < len; i++) {
+    printf("%s", unaPalabra[i]);
+  }
+}
+
+//Revisa si la palbra es palíndroma
+void esPalindroma(char palindroma[][256]) {
+  int haciaDerecha = 0;
+  int len = sizeof(palindroma)/sizeof(char);
+  int haciaIzquierda = len - 1;
+
+  while (haciaIzquierda > haciaDerecha) {
+    if (palindroma[haciaDerecha++] != palindroma[haciaIzquierda--]) {
+      imprimir(palindroma);
+      printf(" no es palíndroma\n");
+      return;
     }
+  }
+  imprimir(palindroma);
+  printf(" es palíndroma\n");
+}
+
+//creador de palabras
+void creaPalabras() {
+  char primera[256][256]; //El nodo raíz siempre será el mismo así que se guarda como constante
+  strcpy(primera[0], palabra[0]);
+
+  char palabraPaldinroma[256][256]; //Se crea la variable para guardar la palabra hecha
+
+  int cont = 1; //Contador para que empiece a crear palabras a partir del segundo
+
+  while (!strcmp(palabra[cont], "!")) { //Mientras que no encuentre un "!" en el array, seguirá agregando letras al array
+    strcpy(palabraPaldinroma[cont], palabra[cont]);
+    cont++;
+  }
+
+  esPalindroma(palabraPaldinroma);
 }
 
 int main(void) {
 
+
   listar("test-directorios", 0);
+
+  for (int i = 0; i < 20; i++) {  //Como se ve el array después del listar
+    printf("Letra: %s\n", palabra[i]);
+  }
+
+  creaPalabras();
 
   return 0;
 }
